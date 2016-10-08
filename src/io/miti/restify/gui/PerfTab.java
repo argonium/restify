b63fcd6ad30dad39d6dff5b84c415bb4d3952a31
Mike Wallace <mfwallace@gmail.com>
2016-10-08 12:52:05 -0400

    More UI updates to the Performance tab.

--- src/io/miti/restify/gui/PerfTab.java
+++ src/io/miti/restify/gui/PerfTab.java
@@ -1,27 +1,109 @@
 package io.miti.restify.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
 
 public class PerfTab extends JPanel
 {
   private static final long serialVersionUID = 1L;
 
   public PerfTab() {
-    super(new BorderLayout());
-    buildPage();
+    super(new GridLayout(2, 1, 20, 20));
+    setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+    buildTopPanel();
+    buildBottomPanel();
   }
   
-  private void buildPage() {
+  private void buildBottomPanel() {
+    
+    JPanel panel = new JPanel(new GridBagLayout());    
+    
+    GridBagConstraints c = new GridBagConstraints();
+    c.gridx = 0;
+    c.gridy = 0;
+    c.gridwidth = 4;
+    c.gridheight = 6;
+    c.insets = new Insets(5, 4, 5, 4);
+    c.anchor = GridBagConstraints.LINE_START;
+    
+    JTextArea taOutput = new JTextArea(8, 25);
+    taOutput.setEditable(false);
+    panel.add(new JScrollPane(taOutput), c);
+    
+    c.gridwidth = 1;
+    c.gridheight = 1;
+    c.gridx = 4;
+    panel.add(new JLabel("Progress:"), c);
+    
+    c.gridx = 5;
+    JTextField tfProgress = new JTextField(6);
+    tfProgress.setEditable(false);
+    panel.add(tfProgress, c);
+    
+    c.gridx = 6;
+    panel.add(new JLabel("Failure Rate:"), c);
+    
+    c.gridx = 7;
+    JTextField tfFailure = new JTextField(6);
+    tfFailure.setEditable(false);
+    panel.add(tfFailure, c);
+    
+    c.gridy = 1;
+    c.gridx = 4;
+    panel.add(new JLabel("Min Resp Time:"), c);
+    
+    c.gridx = 5;
+    JTextField tfMinTime = new JTextField(6);
+    tfMinTime.setEditable(false);
+    panel.add(tfMinTime, c);
+    
+    c.gridx = 6;
+    panel.add(new JLabel("Max Resp Time:"), c);
+    
+    c.gridx = 7;
+    JTextField tfMaxTime = new JTextField(6);
+    tfMaxTime.setEditable(false);
+    panel.add(tfMaxTime, c);
+    
+    c.gridy = 2;
+    c.gridx = 4;
+    panel.add(new JLabel("Worst URL:"), c);
+    
+    c.gridx = 5;
+    c.gridwidth = 3;
+    JTextField tfWorstUrl = new JTextField(20);
+    tfWorstUrl.setEditable(false);
+    panel.add(tfWorstUrl, c);
+    
+    c.gridy = 3;
+    c.gridx = 4;
+    c.gridwidth = 1;
+    panel.add(new JLabel("Worst Time:"), c);
+    
+    c.gridx = 5;
+    JTextField tfWorstTime = new JTextField(6);
+    tfWorstTime.setEditable(false);
+    panel.add(tfWorstTime, c);
+    
+    panel.setBorder(new TitledBorder(BorderFactory.createLineBorder(Color.BLACK), "Results"));
+    
+    add(panel);
+  }
+  
+  private void buildTopPanel() {
     
     JPanel panel = new JPanel(new GridBagLayout());    
     
@@ -95,7 +177,8 @@ public class PerfTab extends JPanel
     JButton btnImport = new JButton("Import HAR");
     panel.add(btnImport, c);
     
+    panel.setBorder(new TitledBorder(BorderFactory.createLineBorder(Color.BLACK), "URLs and Settings"));
     
-    add(panel, BorderLayout.CENTER);
+    add(panel);
   }
 }
